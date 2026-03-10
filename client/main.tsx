27b5f33ddfe4b16054b3ed28cc7485f3797171e8
Safdar Shukur <shuk5741@mylaurier.ca>
2026-03-10 07:58:32 +0000

    Fix runtime error with React StrictMode and dependency deduplication

--- client/main.tsx
+++ client/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./global.css";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -6,5 +7,9 @@ const container = document.getElementById("root");
 
 if (container) {
   const root = createRoot(container);
-  root.render(<App />);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
