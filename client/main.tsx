bf3245243f3b17989c9d78ee707f3487bcfdefa3
Safdar Shukur <shuk5741@mylaurier.ca>
2026-03-21 03:03:55 -0400

    Refactor project structure: rename package and remove Vercel analytics and speed insights dependencies
    Changes Made:
    - Updated dependencies in package.json to reflect the removal of Vercel packages

--- client/main.tsx
+++ client/main.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import "./global.css";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { injectSpeedInsights } from "@vercel/speed-insights";
-import { inject } from "@vercel/analytics";
-
-injectSpeedInsights();
-inject();
 
 const container = document.getElementById("root");
 
